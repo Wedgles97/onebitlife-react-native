7925da3a5e21e7e750005f18601d4e62f1f88a22
wedgles97 <wedegles@gmail.com>
2022-11-28 22:49:10 -0300

    chore: start page styling

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -3,17 +3,21 @@ import {
   View, 
   Text, 
   Image, 
-  ScrollView 
+  ScrollView, 
+  StyleSheet
 } from "react-native";
 
 export default function Start() {
   return (
-    <View>
+    <View style={styles.container}>
       <ScrollView showVerticalScrollIndicator={false}>
-        <View>
-          <Image source={require("../../assets/icons/logo3.png")} />
-          <Text>
-            Vamos transformar sua vida {"/n"} em jogo, buscando sempre {"/n"} o melhor nível.
+        <View style={{alignItems: "center"}}>
+          <Image 
+            style={styles.logo} 
+            source={require("../../assets/icons/logo3.png")} 
+          />
+          <Text style={styles.description}>
+            Vamos transformar sua vida {"\n"} em jogo, buscando sempre {"\n"}{" "} o melhor nível.
           </Text>
         </View>
       </ScrollView>
@@ -21,3 +25,22 @@ export default function Start() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "rgba(21, 21, 21, 0.98)",
+  },
+  logo: {
+    width: 300,
+    height: 60,
+    marginTop: 60,
+    marginBottom: 20,
+  },
+  description: {
+    color: "#ffffff",
+    fontSize: 20,
+    textAlign: "center",
+    marginVertical: 60,
+  }
+})
