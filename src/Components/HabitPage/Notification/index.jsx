a1d7ec6da011215af929b958b1ab2b0841ade19e
wedgles97 <wedegles@gmail.com>
2022-12-01 20:01:00 -0300

    chore: creation of the Notification component

---
+++ src/Components/HabitPage/Notification/index.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  StyleSheet,
+  Switch,
+  Text,
+  View
+} from "react-native";
+
+export default function Notification({ notificationToggle, setNotificationToggle }) {
+  const toggleSwitch = () => {
+    setNotificationToggle((previousState) => !previousState);
+  }
+  return (
+    <>
+      <View style={styles.container}>
+        <Text style={styles.title}>Notificação</Text>
+        <Switch
+          trackColor={{ false: "#ff0044", true: "#2dbe56" }}
+          thumbColor={"#ffffff"}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={notificationToggle}
+        />
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  titleDisabled: {
+    color: "#bbbb",
+    fontSize: 20,
+    marginRight: 10,
+  },
+  title: {
+    color: "#ffffff",
+    fontSize: 20,
+    marginRight: 10,
+  },
+});
