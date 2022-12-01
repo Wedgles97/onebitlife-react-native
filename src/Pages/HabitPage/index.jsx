54b0c58e45ea1b5981f08c057412d55f327cc52b
wedgles97 <wedegles@gmail.com>
2022-11-30 23:48:40 -0300

    chore: creation of the selecthabit component

--- src/Pages/HabitPage/index.jsx
+++ src/Pages/HabitPage/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, setHabitInput } from "react";
 import {
   Image,
   ScrollView,
@@ -9,8 +9,11 @@ import {
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
+import SelectHabit from "../../Components/HabitPage/SelectHabit";
+
 export default function HabitPage({ route }) {
   const navigation = useNavigation();
+  const [habitInput, setHabitInput] = useState();
   const { create, habit } = route.params;
  
   return (
@@ -32,6 +35,8 @@ export default function HabitPage({ route }) {
             <View style={styles.inputContainer}>
               <Text style={styles.area}>{habit?.habitArea}</Text>
             </View>
+            <Text style={styles.inputText}>Hábito</Text>
+            <SelectHabit habit={habit} habitInput={setHabitInput} />
           </View>
         </View>
       </ScrollView>
